414d0982b41d381546317a1a313df0d58edf35ce
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-04 17:27:01 +0200

    missing documentation added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -44,3 +44,38 @@ export default router;
  *       201:
  *         description: Product created
  */
+
+/**
+ * @swagger
+ * /api/products:
+ * get:
+ * summary: Retrieve a list of all products
+ * tags: [Products]
+ * security:
+ * - bearerAuth: []
+ * responses:
+ * 200:
+ * description: A list of products
+ * content:
+ * application/json:
+ * schema:
+ * type: array
+ * items:
+ * type: object
+ * properties:
+ * _id:
+ * type: string
+ * name:
+ * type: string
+ * costPrice:
+ * type: number
+ * sellingPrice:
+ * type: number
+ * createdAt:
+ * type: string
+ * format: date-time
+ * 401:
+ * description: Not authorized, token failed
+ * 500:
+ * description: Internal server error
+ */
