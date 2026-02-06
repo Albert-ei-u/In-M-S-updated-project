8397c7c4c7674bf0c829b4a3f525e592d68ae4cf
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-06 23:32:16 +0200

    checked

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -20,5 +20,34 @@ export default function Products () {
     } finally{
       setLoading(false);
     }
-  }
+  };
+
+  if (loading) return <p>Loading products...</p>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Cost</th>
+            <th>Selling</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((p) => (
+            <tr key={p._id}>
+              <td>{p.name}</td>
+              <td>{p.quantity}</td>
+              <td>{p.costPrice}</td>
+              <td>{p.sellingPrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
